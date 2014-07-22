e7793cf14b7c6ea5637c8334fee8fa597269ecc1
Alexander Kolosov <akolosov@cs.karelia.ru>
2014-07-22 14:39:52 +0400

    init.el added in spite of .gitignore.

---
+++ init.el
@@ -0,0 +1,18 @@
+;;; init.el --- Where all the magic begins
+;;
+;; Part of Alexander Kolosov's Emacs configuration (based on Emacs Starter Kit)
+;;
+;; This is the first thing to get loaded.
+;;
+
+;; load the starter kit from the `after-init-hook' so all packages are loaded
+(add-hook 'after-init-hook
+ `(lambda ()
+    ;; remember this directory
+    (setq starter-kit-dir
+          ,(file-name-directory (or load-file-name (buffer-file-name))))
+    ;; load up the Emacs config
+    (require 'org)
+    (org-babel-load-file (expand-file-name "kas-emacs.org" starter-kit-dir))))
+
+;;; init.el ends here
